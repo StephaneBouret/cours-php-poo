db75c1e827be0bd6664f8040fef417062f9f86f7
Stéphane Bouret <sbouret@orange.fr>
2025-12-22 15:59:06 +0100

    classes et heritages

--- libraries/models/Article.php
+++ libraries/models/Article.php
@@ -4,46 +4,5 @@ require_once('libraries/models/Model.php');
 
 class Article extends Model
 {
-    /**
-     * Retourne la liste des articles classés par date de création
-     *
-     * @return array
-     */
-    public function findAll(): array
-    {
-        $resultats = $this->pdo->query('SELECT * FROM articles ORDER BY created_at DESC');
-        $articles = $resultats->fetchAll();
-        return $articles;
-    }
-
-    /**
-     * Retourne un article grâce à son identifiant
-     *
-     * @param integer $id
-     * @return void
-     */
-    public function find(int $id)
-    {
-        $query = $this->pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
-
-        // On exécute la requête en précisant le paramètre :article_id 
-        $query->execute(['article_id' => $id]);
-
-        // On fouille le résultat pour en extraire les données réelles de l'article
-        $article = $query->fetch();
-
-        return $article;
-    }
-
-    /**
-     * Supprime un article dans la base grâce à son identifiant
-     *
-     * @param integer $id
-     * @return void
-     */
-    public function delete(int $id): void
-    {
-        $query = $this->pdo->prepare('DELETE FROM articles WHERE id = :id');
-        $query->execute(['id' => $id]);
-    }
+    protected $table = "articles";
 }
--- libraries/models/Model.php
+++ libraries/models/Model.php
@@ -2,12 +2,55 @@
 
 require_once('libraries/database.php');
 
-class Model
+abstract class Model
 {
     protected $pdo;
+    protected $table;
 
     public function __construct()
     {
         $this->pdo = getPdo();
+    }
+
+    /**
+     * Retourne la liste des articles classés par date de création
+     *
+     * @return array
+     */
+    public function findAll(?string $order = ""): array
+    {
+        $sql = "SELECT * FROM {$this->table}";
+        if ($order) {
+            $sql .= " ORDER BY " . $order;
+        }
+        $resultats = $this->pdo->query($sql);
+        $articles = $resultats->fetchAll();
+        return $articles;
+    }
+
+    /**
+     * Retourne un article grâce à son identifiant
+     *
+     * @param integer $id
+     * @return void
+     */
+    public function find(int $id)
+    {
+        $query = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE id = :id");
+        $query->execute(['id' => $id]);
+        $item = $query->fetch();
+        return $item;
+    }
+
+    /**
+     * Supprime un commentaire grâce à son identifiant
+     *
+     * @param integer $id
+     * @return void
+     */
+    public function delete(int $id): void
+    {
+        $query = $this->pdo->prepare("DELETE FROM {$this->table} WHERE id = :id");
+        $query->execute(['id' => $id]);
     }
 }
